5c074c982af2f2253fe9ef595f8cf85bd30abe31
Edsel Serrano <pretxel@hotmail.com>
2013-06-25 01:24:02 -0500

    cambio en imagenes

--- sendmail_comentario.php
+++ sendmail_comentario.php
@@ -3,12 +3,15 @@ include("class.phpmailer.php");
 include("class.smtp.php");
 
 	if (count($_POST) > 0){
+		$nombre = $_POST['nombre'];
 		$email = $_POST['email'];
+		$comentario = $_POST['comentario'];
 		$header = "Content-Type: text/html\r\nReply-To: $email\r\nFrom:";
 		
 		$body = @"Correo enviado desde: ".$_SERVER['REMOTE_ADDR']." el d&iacutea ".date("d/m/Y M:i", time())."<br />
 			<hr />
-			Hola me interesa Klinco, mi correo es: $email 
+			Hola mi nombre es $nombre y mi correo es $email<br>
+			Tengo algunos comentarios: $comentario
 			<br>
 			¡Saludos!
 			<hr />
@@ -23,13 +26,13 @@ include("class.smtp.php");
 		$mail->SMTPAuth = true;     // cambia autenticacion
 		$mail->Username = "pretxel100@gmail.com";  // SMTP nombre usuario
 		$mail->Password = "ESCOM2010%"; // SMTP contraseña
-		
+			
 		//$mail->AddAddress("hello@klinco.co", "Klinco");	
 		$mail->AddAddress("pretxel100@hotmail.com", "Klinco");
 		$mail->WordWrap = 50;
 		$mail->From = $email;
 		$mail->IsHTML(true);	
-		$mail->Subject = "Klincofriend!";
+		$mail->Subject = "Comentarios Klinco!";
 		$mail->Body    = $body;
 		$mail->AltBody = $body;	
 			
